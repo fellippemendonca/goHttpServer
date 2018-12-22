ecc7db71f05e7a57abd6c78f19c1965a75f74947
fellippemendonca <fellippe.mendoca@thinxnet.com>
2018-12-22 19:06:59 +0100

    First commit

---
+++ goHttpServer.go
@@ -0,0 +1,9 @@
+package main;
+
+import (
+	"github.com/fellippemendonca/goHttpServer/lib"
+)
+
+func main() {
+	lib.HttpServer();
+}
---
+++ lib/httpServer.go
@@ -0,0 +1,27 @@
+package lib;
+
+import (
+	"io";
+	"fmt";
+	"log";
+	"net/http";
+)
+
+func HttpServer() {
+	fmt.Println("\n\n ## The following content is exploring Go Http Server ## \n");
+	initServer();
+}
+
+func initServer() {
+	http.HandleFunc("/", homeHandler);
+	http.HandleFunc("/hello", helloHandler);
+	log.Fatal(http.ListenAndServe(":8080", nil));
+}
+
+func homeHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Welcome to home!\n");
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world!\n");
+}
