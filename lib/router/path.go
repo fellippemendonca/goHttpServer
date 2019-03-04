681a5a003d91e824fa664f9ebe1bacb4dcf35658
fellippe <fellippe.mendonca@gmail.com>
2019-03-04 20:50:02 +0100

    Updates

--- lib/router/path.go
+++ lib/router/path.go
@@ -3,8 +3,6 @@ package router
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/fellippemendonca/goHttpServer/lib/logger"
 )
 
 // NewPath to be handled by the Mux handler
@@ -34,17 +32,25 @@ func (p *Path) Add(u string) *Path {
 func (p *Path) NewHandler() *Handler {
 	h := NewHandler()
 	h.route = p.resolveRoute()
-	p.mux.Handle(h.route+"/", p.use(h))
+	// p.mux.Handle(h.route+"/", p.use(h))
+	p.mux.Handle(h.route+"/", use(h))
 	fmt.Println(h.route + "/")
 	return h
 }
 
+func use(next http.Handler) http.Handler {
+	fmt.Println("USE!!!")
+	return next
+}
+
+/*
 func (p *Path) use(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		logger.Info("URI:", r.RequestURI, ", Headers:", r.Header.Get("x-auth-token"))
 		next.ServeHTTP(w, r)
 	})
 }
+*/
 
 func (p *Path) resolveRoute() string {
 	if p.head != nil {
